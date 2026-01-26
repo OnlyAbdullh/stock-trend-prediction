e834faba89951c3e5635a168fde7b0b5e3473dab
abdulrahman_204 <abdalrahmanaljomaat@gmail.com>
2026-01-26 17:39:03 +0300

    remove general gaps

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,10 +2,11 @@ from pathlib import Path
 import logging
 import click
 import pandas as pd
+import numpy as np
 
 
-def clean_stock_df(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.copy()
+def verify_columns_and_types(df: pd.DataFrame) -> pd.DataFrame:
+
     df.columns = (
         df.columns
         .str.strip()
@@ -35,7 +36,6 @@ def handle_missing_values(
         price_cols=("open", "high", "low", "close"),
         max_na_fraction=0.10,
 ) -> pd.DataFrame:
-    df = df.copy()
     price_cols = list(price_cols)
 
     removed_tickers = 0
@@ -75,14 +75,13 @@ def handle_missing_values(
         .groupby("ticker", group_keys=False)
         .apply(process_ticker)
     )
-    return df_clean
+    return df_clean # TODO : check this
 
 
 def drop_ticker_date_duplicates(
     df: pd.DataFrame,
     max_duplicates_per_ticker: int = 10
 ) -> pd.DataFrame:
-    df = df.copy()
     dup_counts = (
         df.groupby(["ticker", "date"])
         .size()
@@ -99,7 +98,6 @@ def drop_ticker_date_duplicates(
     return df
 
 def remove_invalid_rows(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.copy()
 
     cond_open = df["open"] != 0
     cond_close = df["close"] != 0
@@ -108,13 +106,10 @@ def remove_invalid_rows(df: pd.DataFrame) -> pd.DataFrame:
     cond_volume = df["volume"] > 0
     valid_mask = cond_open & cond_close & cond_high_low & cond_open_range & cond_volume
 
-    removed = (~valid_mask).sum()
     return df[valid_mask].copy()
 
 def filter_by_start_date(df: pd.DataFrame, start_date: str) -> pd.DataFrame:
-    df = df.copy()
-    df = df[df["date"] >= start_date]
-    return df
+    return df[df["date"] >= start_date]
 
 def remove_corrupted_tickers_df(
     df: pd.DataFrame,
@@ -126,7 +121,6 @@ def remove_corrupted_tickers_df(
     يحسب العوائد + القيم المتطرفة لكل سهم داخلياً،
     ثم يحذف الأسهم التي نسبة القيم المتطرفة فيها تتجاوز threshold٪.
     """
-    df = df.copy()
     df = df.sort_values(["ticker", "date"])
 
     df["return"] = (
@@ -176,15 +170,78 @@ def remove_corrupted_tickers_df(
 
 
 def run_basic_clean_df(df_raw: pd.DataFrame) -> pd.DataFrame:
-    df = clean_stock_df(df_raw)
+    df = verify_columns_and_types(df_raw)
+    df = filter_after_2010_df(df)
     df = handle_missing_values(df)
     df = remove_invalid_rows(df)
     df = drop_ticker_date_duplicates(df)
+    df = remove_global_gaps(df)
     return df
 
 def filter_after_2010_df(df: pd.DataFrame) -> pd.DataFrame:
     return filter_by_start_date(df, "2010-01-01")
 
+def remove_global_gaps(df: pd.DataFrame) -> pd.DataFrame:
+
+    df = df.sort_values(["ticker", "date"]).copy()
+
+    df["prev_date"] = df.groupby("ticker")["date"].shift(1)
+    df["gap_days"] = (df["date"] - df["prev_date"]).dt.days
+
+    # gaps = df[df["gap_days"] > MAX_NORMAL_GAP].copy()
+    df["missing_days"] = df["gap_days"] - 1
+    gaps = df[df["missing_days"] >= 1].copy()
+    gap_counts = (
+        gaps.groupby("date")
+        .size()
+        .rename("gap_tickers")
+    )
+    data_counts = (
+        df.groupby("date")
+        .size()
+        .rename("total_tickers")
+    )
+    gap_vs_data = (
+        gap_counts
+        .to_frame()
+        .join(data_counts, how="left")
+        .fillna(0)
+    )
+
+    gap_vs_data["gap_ratio"] = (
+            gap_vs_data["gap_tickers"] / gap_vs_data["total_tickers"]
+    )
+    GLOBAL_THRESHOLD = 0.8
+
+    gap_vs_data["gap_type"] = np.where(
+        gap_vs_data["gap_ratio"] >= GLOBAL_THRESHOLD,
+        "global",
+        "local"
+    )
+    global_gap_dates = gap_vs_data.index[
+        gap_vs_data["gap_type"] == "global"
+        ]
+    df_no_global_gaps = df[~df["date"].isin(global_gap_dates)].copy()
+
+    local_gaps = gaps[
+        ~gaps["date"].isin(global_gap_dates)
+    ][["date", "ticker"]]
+    logging.info(
+        "Saving final cleaned data (after removing corrupted tickers) to %s",
+        Path('data/interim/local_gaps.csv')
+
+    )
+    # fill missing_days NaNs
+    df_no_global_gaps["missing_days"] = df_no_global_gaps["missing_days"].fillna(0).astype(int)
+
+    # drop helper columns
+    df_no_global_gaps = df_no_global_gaps.drop(
+        columns=["prev_date", "gap_days"],
+        errors="ignore"
+    )
+
+    local_gaps.to_csv('data/interim/local_gaps.csv', index=False)
+    return df_no_global_gaps
 
 @click.command()
 @click.argument("input_filepath", type=click.Path(exists=True))
@@ -201,14 +258,16 @@ def main(input_filepath: str):
     df = pd.read_csv(input_path)
     # basic clean
     df = run_basic_clean_df(df)
-    basic_clean_path = interim_dir / "train_clean_basic.csv"
-    logging.info("Saving basic cleaned data to %s", basic_clean_path)
-    df.to_csv(basic_clean_path, index=False)
-    # بعد 2010
-    df = filter_after_2010_df(df)
-    after_2010_path = interim_dir / "train_clean_after_2010.csv"
-    logging.info("Saving data after 2010-01-01 to %s", after_2010_path)
-    df.to_csv(after_2010_path, index=False)
+    # basic_clean_path = interim_dir / "train_clean_basic.csv"
+    # logging.info("Saving basic cleaned data to %s", basic_clean_path)
+    # df.to_csv(basic_clean_path, index=False)
+    # # بعد 2010
+    # df = filter_after_2010_df(df) # moved to basic_clean_run
+
+    # not necessary now
+    # after_2010_path = interim_dir / "train_clean_after_2010.csv"
+    # logging.info("Saving data after 2010-01-01 to %s", after_2010_path)
+    # df.to_csv(after_2010_path, index=False)
     # إزالة الأسهم الفاسدة
     df, removed_tickers = remove_corrupted_tickers_df(
         df,
